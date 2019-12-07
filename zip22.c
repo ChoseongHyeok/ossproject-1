74d817934d2a12bd59d3f8e372c87f9695e31093
ChoseongHyeok <secho6609@naver.com>
2019-12-07 10:32:59 +0000

    zip2 code

---
+++ zip22.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdilb.h>
+
+int main()
+{
+	FILE *fd;
+	int stat;
+	int i=1;
+
+	system("mkdir ZIP");
+	system("unzip *.zip -d /home/ZIP");
+	system("cd ZIP");
+
+	if(val == no);
+	{
+		printf("File not exist");
+	}
+	fp = popen("find val.*","r");
+	
+	state = pclose(fp);
+	return 0;
+
+
+
